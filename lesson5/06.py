f47e22aaa7aa1094bafdfbf6d7497fc14c9105ff
zerospirit79 <pavel.s.shilov@gmail.com>
2020-10-06 12:29:00 +0300

    lesson5 updated part 6

--- lesson5/06.py
+++ lesson5/06.py
@@ -13,8 +13,28 @@
 """
 import json
 profit = {}
+profit1 = {}
+profit2 = 0
+average_profit = 0
+i = 0
 with open('06.txt', 'r') as my_file:
     for line in my_file:
         name, company, profed, loss = line.split()
         profit[name] = int(profed) - int(loss)
-        if profit.setdetault(name) >= 0:
+        if profit.setdefault(name) >= 0:
+            profit2 = profit2 + profit.setdefault(name)
+            i += 1
+    if i != 0:
+        average_profit = profit2 / i
+        print(f"Средняя прибыль - {average_profit:.2f}")
+    else:
+        print(f"Прибыль средняя отсутствует. Компании работают в убыток")
+    profit1 = {'Средняя прибыль': round(average_profit)}
+    profit.update(profit1)
+    print(f'Прибыль каждой компании - {profit1}')
+
+with open('file_07.txt', 'w') as new_file:
+    json.dump(profit, new_file)
+
+    js_string = json.dumps(profit)
+    print(f'Создан json файл с содержанием \n' f'{js_string}')
